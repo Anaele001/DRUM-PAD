fc92b3566bd2139cf1bd5f6ce34135ac99e9a661
Anaele <anaelemichael9@gmail.com>
2024-03-21 18:41:54 +0100

    My drum pad

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const mainaBody = document.querySelector("body");
+mainaBody.classList.add("body-1")
+function changeBg() {
+    const mainaBody = document.querySelector("body");
+    if (mainaBody.classList.contains("body-1")) {
+        mainaBody.classList.remove("body-1")
+        mainaBody.classList.add("body-2");
+    } else if (mainaBody.classList.contains("body-2")) {
+        mainaBody.classList.remove("body-2")
+        mainaBody.classList.add("body-3");
+    } else {
+        mainaBody.classList.remove("body-3")
+        mainaBody.classList.add("body-1");
+    }
+}
+
+setInterval(changeBg, 4000);
+
+const audio = document.querySelector("audio")
+audio.play();
+
