049d763b0fd13f07bad070e0efa21d4566277a5b
Anaele <anaelemichael9@gmail.com>
2024-03-23 18:58:28 +0100

    updated my drum pad

--- js/main.js
+++ js/main.js
@@ -16,6 +16,13 @@ function changeBg() {
 
 setInterval(changeBg, 4000);
 
-const audio = document.querySelector("audio")
-audio.play();
+window.addEventListener("keydown", function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+    if (!audio) {
+        return;
+    }
+
+    audio.play();
+
+})
 
